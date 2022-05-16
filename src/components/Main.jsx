b36b35972ce84bdb794a5d9fa02bbfc9405ded40
anandpatel48 <anandrpatel48@gmail.com>
2022-05-16 16:31:21 -0400

    concluding commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,12 +7,13 @@ function Main(props) {
             
             <Nav />
             <h1>Main</h1>
-            <p>This app will allow a user to see the game schedule for a certain day for the NBA.
-                The app will also allows the user to search for games in the NBA by a certain date.
-                Go to the NBA page and input a date using the same format already seen in the search box
-                and search for the game.
+            <p>This app will allow a user to see the game schedule for a certain day for the NBA or MLB.
+                The app will also allows the user to search for games in the NBA or MLB by a certain date.
+                Go to the NBA or MLB page and input a date using the same format already seen in the search box
+                and search for the game. Data is from Sportsdata.io.
             </p>
             <img className = "mainImg" src = "https://andscape.com/wp-content/uploads/2017/06/nbalogo.jpg?w=700" alt = "nba logo" />
+            <img className = "mainImg" src = "https://images.squarespace-cdn.com/content/v1/58ee0b551e5b6c8ff18b94ad/1621939471446-R9E5H71EHYZHT2WRD6L6/MLB+Logo.png" alt = "nba logo" />
         </div>
         )
 }
