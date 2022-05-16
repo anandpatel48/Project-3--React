9ae61d6691c658c7fe98c7b518098263703d87f1
anandpatel48 <anandrpatel48@gmail.com>
2022-05-16 10:42:46 -0400

    got schedule display to work

--- src/components/NBA.jsx
+++ src/components/NBA.jsx
@@ -1,28 +1,49 @@
 
 import {useState, useEffect} from 'react'
 
+
 function NBA() {
-  const [data, setData] = useState(null)
-  useEffect(() => {
-    async function fetchData() {
-        let res = await fetch('https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/2022-MAY-15?key=08e1666ff8dc4172868e02bb673ff311')
-        let data = await res.json();
-        setData(data)
+    const month = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+    const [date, setDate] = useState(new Date())
+    // console.log(date)
+
+    const dateQuery = `${date.getFullYear()}-${month[date.getMonth()]}-${date.getDate()}`
+    // console.log(dateQuery)
+
+    const [data, setData] = useState([])
+    useEffect(() => {
+      async function fetchData() {
+          let res = await fetch(`https://api.sportsdata.io/v3/nba/scores/json/GamesByDate/${dateQuery}?key=08e1666ff8dc4172868e02bb673ff311`)
+          let data = await res.json();
+          setData(data)
+      }
+      fetchData()
+      
+  },[])
+//   console.log(data)
+  
+//   makeDate()
+
+
+const showSchedule = data.map((game, idx) => {
+    if(data.length === 1 && data[0].Status === 'Canceled') {
+        return (
+            <h1 key = {idx}>No Games Scheduled for Today</h1>
+        )
+    } else if (game.Status !== 'Canceled') {
+        return (
+            <div key={idx}>
+                <h1>{`${game.AwayTeam} @ ${game.HomeTeam}`}</h1>
+            </div>
+        )
     }
-    fetchData()
-    
-},[])
-
-
-
-
-
-console.log(data)
+    }
+)
 
   return (
     <div className="App">
-      <h1>Test</h1>
-      <h2>{data[0].GameID}</h2>
+      <h1>Games Today</h1>    
+      {showSchedule}
     </div>
   );
 }
